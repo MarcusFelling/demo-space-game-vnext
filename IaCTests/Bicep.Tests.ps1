bcb331f40e4613dc522f40a2d273aedac2120b14
Marcus Felling <MarcusFelling@users.noreply.github.com>
2021-07-06 18:24:02 -0500

    Add Pester tests for IaC (#12)
    * Add pester tests
    Co-authored-by: Marcus Felling <MarcusFelling@microsoft.com>

---
+++ IaCTests/Bicep.Tests.ps1
@@ -0,0 +1,30 @@
+Describe 'Bicep Template Validation' {
+    Context 'Template File Validation' {
+        It "Transpiled ARM Template File Exists" {
+            Test-Path -Path ".\IaC\main.json" | Should -BeTrue
+        }
+        It "Transpiled ARM Template is a valid json file" {
+            Get-Content ".\IaC\main.json" -raw | ConvertFrom-json -ErrorAction SilentlyContinue | Should -Not -Be $null
+        }
+    }
+    Context 'Template Content Validation' {
+        $Resources = @(
+            'Microsoft.Sql/servers',
+            'Microsoft.Sql/servers/databases',
+            'icrosoft.Sql/servers/firewallRules',
+            'Microsoft.ContainerRegistry/registries',
+            'Microsoft.Web/serverfarms',
+            'Microsoft.Web/sites',
+            'Microsoft.Web/sites/config',
+            'Microsoft.Web/sites/slots',
+            'Microsoft.Insights/components'
+        )
+        It "Only has approved resources" {
+            $resourcesFromTemplate = $TemplateJson.resources.type | Sort-Object
+            $strResourcesFromTemplate = $resourcesFromTemplate -join ','
+            $resources = $resources | Sort-Object
+            $strResources = $resources -join ','
+            $strResourcesFromTemplate | Should -be $strResources
+        }
+    }
+}

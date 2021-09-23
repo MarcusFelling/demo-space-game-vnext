7d88c962968019df7d698c7b4bd50025e4143c4d
Marcus Felling <MarcusFelling@microsoft.com>
2021-09-23 12:29:18 -0500

    Add Playwright tests

---
+++ PlaywrightTests/Paginate.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+using PlaywrightTests.lib;
+
+namespace PlaywrightTests
+{
+    [Parallelizable(ParallelScope.Self)]
+    public class Paginate : PageTest
+    {
+        [Test]
+        public async Task CanPaginate()
+        {
+            await Helpers.VisitURL(Page);
+
+            // Paginate through leaderboard results
+            await Page.ClickAsync("text=2 (current)");
+            await Page.ClickAsync("text=3 (current)");
+            await Page.ClickAsync("text=1 (current)");
+        }
+    }
+}

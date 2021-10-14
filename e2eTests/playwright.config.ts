555a6469e9af0ebc8609d61d9b830e95b7149049
Marcus Felling <MarcusFelling@microsoft.com>
2021-10-14 15:12:50 -0500

    Use typescript for Playwright tests

---
+++ e2eTests/playwright.config.ts
@@ -0,0 +1,64 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+import path from 'path';
+
+// Reference: https://playwright.dev/docs/test-configuration
+const config: PlaywrightTestConfig = {
+  
+  // Timeout per test
+  timeout: 10 * 1000,
+  // If a test fails, retry it additional 2 times
+  retries: 2,
+  // Artifacts folder where screenshots, videos, and traces are stored.
+  outputDir: 'test-results/',
+
+  use: {
+    // Run headless by default
+    headless: true,
+
+    // Use env var to set baseURL
+    baseURL: process.env.SITE_URL,
+
+    // Retry a test if its failing with enabled tracing. This allows you to analyse the DOM, console logs, network traffic etc.
+    // More information: https://playwright.dev/docs/trace-viewer
+    trace: 'retry-with-trace',
+
+    // All available context options: https://playwright.dev/docs/api/class-browser#browser-new-context
+    contextOptions: {
+      ignoreHTTPSErrors: true,
+    },
+  },
+
+  projects: [
+    {
+      name: 'Desktop Chrome',
+      use: {
+        ...devices['Desktop Chrome'],
+
+      },
+    },
+    {
+      name: 'Desktop Firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+    {
+      name: 'Desktop Safari',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+    // Test against mobile viewports.
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+      },
+    },
+    {
+      name: 'Mobile Safari',
+      use: devices['iPhone 12'],
+    },
+  ],
+};
+export default config;
---
+++ e2eTests/view-screenshots.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('View Screenshot examples', async ({ page }) => {
+  await page.goto('');
+  await page.click('li img');
+  await page.click('text=× Gamescreen example >> button');
+  await page.click('ul >> :nth-match(img, 2)');
+  await page.click('text=× Gamescreen example >> button');
+  await page.click('ul >> :nth-match(img, 3)');
+  await page.click('text=× Gamescreen example >> div');
+  await page.click('text=× Gamescreen example >> button');
+});

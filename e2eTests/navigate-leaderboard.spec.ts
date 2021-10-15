76d42436c63d67d539bc739f910b401ae3ce5e60
Marcus Felling <MarcusFelling@microsoft.com>
2021-10-15 15:17:25 -0500

    Make sure profile has at least 1 achievement

--- e2eTests/navigate-leaderboard.spec.ts
+++ e2eTests/navigate-leaderboard.spec.ts
@@ -5,14 +5,18 @@ test('Navigate leadboard', async ({ page }) => {
 
   // Leaderboard section should have Space leaders header
   await expect(page.locator('section.leaderboard > div > h2')).toHaveText('Space leaders');
-
+  
   // Click #1 ranked profile
   await page.click('[data-target="#profile-modal-1"]');
 
   // Make sure profile is ranked #1
-  await page.click('text=Rank #1'); 
-
-  // Close modal
+  await page.click('text=Rank #1');
+  
+  // Make sure profile has at least 1 achievement
+  const length = await page.$$eval('div.modal-body > div > div.col-sm-8 > div > ul', (items) => items.length);
+  expect(length >= 1).toBeTruthy();
+  
+  // Close profile modal
   await page.click('[data-dismiss="modal"]');
 
   // Paginate results

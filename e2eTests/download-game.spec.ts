622868c75d13c679dbfae481d51920173839c210
Marcus Felling <MarcusFelling@microsoft.com>
2021-11-17 13:22:24 -0600

    test download

--- e2eTests/download-game.spec.ts
+++ e2eTests/download-game.spec.ts
@@ -1,8 +1,17 @@
 import { test, expect } from '@playwright/test';
 
+// Browser context must be created with the acceptDownloads set to true access to the downloaded content. 
+test.use({
+  acceptDownloads: true
+});
+
 test('Download game', async ({ page }) => {
   await page.goto('');
-  await page.click('text=Download game');
-  await page.click('text=This link is for example purposes and goes nowhere. 😐');
-  await page.click('#pretend-modal >> text=×');
+
+  const [ download ] = await Promise.all([
+    page.waitForEvent('download'), // wait for download to start
+    page.click('text=Download game')
+  ]);
+  // wait for download to complete
+  const path = await download.path();
 });

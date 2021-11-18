49e5d2ad0933182e1f0d96c38314e8d1c893e6e2
Marcus Felling <MarcusFelling@microsoft.com>
2021-11-18 08:44:26 -0600

    revert testing changes

--- e2eTests/view-screenshots.spec.ts
+++ e2eTests/view-screenshots.spec.ts
@@ -4,7 +4,7 @@ test('View game screenshot examples', async ({ page }) => {
   await page.goto('');
   
   // Loop through each game screenshot example
-  for (const screenshot of ['creenshot-1', 'screenshot-2', 'screenshot-3', 'screenshot-4']) {
+  for (const screenshot of ['screenshot-1', 'screenshot-2', 'screenshot-3', 'screenshot-4']) {
     
     // Click each screenshot example using data-test-id
     await page.click("data-test-id=" + screenshot);

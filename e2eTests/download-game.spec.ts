36aa7cf11aa98c7034e52b0a3f489cd0cc805ae8
Marcus Felling <MarcusFelling@microsoft.com>
2021-12-31 11:20:20 -0600

    wait for download

--- e2eTests/download-game.spec.ts
+++ e2eTests/download-game.spec.ts
@@ -5,7 +5,8 @@ test('Download game', async ({ page }) => {
 
   const [ download ] = await Promise.all([
     page.waitForEvent('download'), // wait for download to start
-    page.locator('text=Download game').click()
+    page.locator('text=Download game').click(),
+    page.locator('text=Fake Game Collection 1.9.3').click()
   ]);
   // wait for download to complete
   const path = await download.path();

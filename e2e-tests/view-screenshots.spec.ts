5215154870da91a293e5ef696754dc75d57eeadb
Marcus Felling <marcus.felling@microsoft.com>
2022-06-06 20:51:41 -0500

    Update view-screenshots.spec.ts

--- e2e-tests/view-screenshots.spec.ts
+++ e2e-tests/view-screenshots.spec.ts
@@ -7,7 +7,7 @@ test('View game screenshot examples', async ({ page }) => {
   for (const screenshot of ['screenshot-1', 'screenshot-2', 'screenshot-3', 'screenshot-4']) {
     
     // Click each screenshot example using data-test-id
-    await page.locator("data-test-id=" + screenshot).click();
+    await page.locator("data-test-id=" + screenshot)
     
     // Close screenshot
     await page.locator('text=× Gamescreen example >> button.close').click(); 
